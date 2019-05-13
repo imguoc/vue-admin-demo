62d954d7016a5a574e6643df2bbc1f47980011e3
IMGUOC <751710930@qq.com>
2019-05-13 11:32:02 +0800

    update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/vue-admin-demo/dist/',
 
     /**
      * Source Maps

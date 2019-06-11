7daac63d5482124f79ded83ced7ad218f7e8865d
IMGUOC <751710930@qq.com>
2019-06-11 15:24:27 +0800

    build

---
+++ dist/static/js/3.a69431ceb78b6b273f22.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{KR8f:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var c={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"layout-box flex-item"},[this._v("\n    welcome!\n")])},staticRenderFns:[]};var a=n("VU/8")({name:"Home"},c,!1,function(e){n("tc8S")},"data-v-8430cace",null);t.default=a.exports},tc8S:function(e,t){}});
+//# sourceMappingURL=3.a69431ceb78b6b273f22.js.map

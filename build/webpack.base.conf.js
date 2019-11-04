a021cfcf1c6b608086c8dbb81f3170b67890e993
IMGUOC <751710930@qq.com>
2019-11-04 14:20:16 +0800

    'update'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -42,6 +42,17 @@ module.exports = {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
+          'sass-loader'
+        ]
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
